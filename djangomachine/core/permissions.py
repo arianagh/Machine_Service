16f2730a6181fd9c7dea786c44be396e7e5712bb
arianagh <arian1999@protonmail.com>
2022-06-12 18:50:17 +0430

    panel admin and models

---
+++ djangomachine/core/permissions.py
@@ -0,0 +1,18 @@
+from rest_framework.permissions import IsAuthenticated
+
+
+class IsTechnician(IsAuthenticated):
+    def has_permission(self, request, view):
+        return super().has_permission(request, view) and request.user.role == 'Technician'
+
+
+class IsInspector(IsAuthenticated):
+    def has_permission(self, request, view):
+        return super().has_permission(request, view) and request.user.role == 'Inspector'
+
+
+class IsReception(IsAuthenticated):
+    def has_permission(self, request, view):
+        return super().has_permission(request, view) and request.user.role == 'Reception'
+
+
---
+++ djangomachine/technician/serializers.py
@@ -0,0 +1,4 @@
+from django.contrib.auth import get_user_model
+from django.shortcuts import get_object_or_404
+from django.utils.timezone import now
+from rest_framework import serializers
